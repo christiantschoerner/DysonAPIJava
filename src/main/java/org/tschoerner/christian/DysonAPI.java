3a4d849a1848ff13fab5e8cba5a35e671a9df708
BuildTools <unconfigured@null.spigotmc.org>
2019-11-30 19:08:38 +0100

    Dyson 455 (Hot + Cool added) - Some fixes

--- src/main/java/org/tschoerner/christian/DysonAPI.java
+++ src/main/java/org/tschoerner/christian/DysonAPI.java
@@ -10,14 +10,5 @@ public class DysonAPI {
             return;
         }
 
-        final DysonDevice475 dyson = new DysonDevice475("XXX-XX-XXXXXXXX",
-                "1.1.1.1",
-                "ABCDEFGHIJKLMNOPQRTUVWXYZ");
-        dyson.connect();
-        dyson.turnOn();
-        dyson.setFanLevel(3);
-        dyson.setOscillation(true);
-        dyson.setNightmode(false);
-        dyson.disconnect();
     }
 }
